f399387c952c65d631b01fa8b0b8e412e0bdedd1
cschaefer26 <c.schaefer.home@gmail.com>
2020-04-16 09:51:33 +0200

    Improved requirements.

--- train_forward.py
+++ train_forward.py
@@ -4,10 +4,11 @@ from torch import optim, nn
 import torch.nn.functional as F
 
 from models.forward_tacotron import ForwardTacotron
+from trainer.forward_trainer import ForwardTrainer
 from utils import hparams as hp
 from utils.dataset import get_tts_datasets
 from utils.display import *
-from utils.text.symbols import symbols
+from utils.text.symbols import phonemes
 from utils.paths import Paths
 from models.tacotron import Tacotron
 import argparse
@@ -18,10 +19,7 @@ import numpy as np
 from utils.checkpoints import save_checkpoint, restore_checkpoint
 
 
-def np_now(x: torch.Tensor): return x.detach().cpu().numpy()
-
-
-def main():
+if __name__ == '__main__':
     # Parse Arguments
     parser = argparse.ArgumentParser(description='Train Tacotron TTS')
     parser.add_argument('--force_train', '-f', action='store_true', help='Forces the model to train past total steps')
@@ -49,7 +47,7 @@ def main():
     # Instantiate Forward TTS Model
     print('\nInitialising Forward TTS Model...\n')
     model = ForwardTacotron(embed_dims=hp.forward_embed_dims,
-                            num_chars=len(symbols),
+                            num_chars=len(phonemes),
                             durpred_rnn_dims=hp.forward_durpred_rnn_dims,
                             durpred_conv_dims=hp.forward_durpred_conv_dims,
                             rnn_dim=hp.forward_rnn_dims,
@@ -68,121 +66,5 @@ def main():
     optimizer = optim.Adam(model.parameters())
     restore_checkpoint('forward', paths, model, optimizer, create_if_missing=True)
 
-    if not force_gta:
-        for i, session in enumerate(hp.forward_schedule):
-            current_step = model.get_step()
-
-            lr, max_step, batch_size = session
-
-            training_steps = max_step - current_step
-
-            simple_table([(f'Steps', str(training_steps//1000) + 'k Steps'),
-                          ('Batch Size', batch_size),
-                          ('Learning Rate', lr)])
-
-            train_set, mel_example = get_tts_datasets(paths.data, batch_size, 1, alignments=True)
-            train_loop(paths, model, optimizer, train_set, lr, training_steps, mel_example)
-
-    train_set, mel_example = get_tts_datasets(paths.data, 8, 1, alignments=True)
-    create_gta_features(model, train_set, paths.gta)
-    print('Training Complete.')
-
-
-def train_loop(paths: Paths, model, optimizer, train_set, lr, train_steps, mel_example):
-    device = next(model.parameters()).device  # use same device as model parameters
-
-    for g in optimizer.param_groups: g['lr'] = lr
-
-    total_iters = len(train_set)
-    epochs = train_steps // total_iters + 1
-
-    for e in range(1, epochs+1):
-
-        start = time.time()
-        running_loss = 0
-        dur_running_loss = 0
-
-        # Perform 1 epoch
-        for i, (x, m, ids, mel_len, dur) in enumerate(train_set, 1):
-
-            x, m, dur = x.to(device), m.to(device), dur.to(device)
-            # Parallelize model onto GPUS using workaround due to python bug
-            if device.type == 'cuda' and torch.cuda.device_count() > 1:
-                m_hat, m_post_hat, dur_hat = data_parallel_workaround(model, x, m, dur)
-            else:
-                m_hat, m_post_hat, dur_hat = model(x, m, dur)
-
-            lin_loss = F.l1_loss(m_hat, m)
-            post_loss = F.l1_loss(m_post_hat, m)
-            dur_loss = F.l1_loss(dur_hat, dur)
-            loss = lin_loss + post_loss + dur_loss
-            optimizer.zero_grad()
-
-            loss.backward()
-
-            if hp.forward_clip_grad_norm is not None:
-                grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), hp.forward_clip_grad_norm)
-                if np.isnan(grad_norm):
-                    print('grad_norm was NaN!')
-
-            optimizer.step()
-
-            running_loss += post_loss.item()
-            avg_loss = running_loss / i
-            dur_running_loss += dur_loss.item()
-            dur_avg_loss = dur_running_loss / i
-
-            speed = i / (time.time() - start)
-
-            step = model.get_step()
-            k = step // 1000
-
-            if step % hp.forward_checkpoint_every == 0:
-                ckpt_name = f'fast_speech_step{k}K'
-                save_checkpoint('forward', paths, model, optimizer,
-                                name=ckpt_name, is_silent=True)
-
-            if mel_example in ids:
-                idx = ids.index(mel_example)
-                try:
-                    seq = x[idx].tolist()
-                    m_gen = model.generate(seq)
-                    save_spectrogram(m_gen, paths.forward_mel_plot / f'{step}_gen', 600)
-                except Exception:
-                    traceback.print_exc()
-                save_spectrogram(np_now(m_post_hat[idx]), paths.forward_mel_plot/f'{step}_gta', 600)
-                save_spectrogram(np_now(m[idx]), paths.forward_mel_plot/f'{step}_target', 600)
-
-            msg = f'| Epoch: {e}/{epochs} ({i}/{total_iters}) | Mel Loss: {avg_loss:#.4} ' \
-                  f'| Duration Loss: {dur_avg_loss:#.4} | {speed:#.2} steps/s | Step: {k}k | '
-            stream(msg)
-        model.log(paths.forward_log, msg)
-
-        save_checkpoint('forward', paths, model, optimizer, is_silent=True)
-
-
-def create_gta_features(model: Tacotron, train_set, save_path: Path):
-    device = next(model.parameters()).device  # use same device as model parameters
-
-    iters = len(train_set)
-
-    for i, (x, mels, ids, mel_lens, dur) in enumerate(train_set, 1):
-
-        x, mels, dur = x.to(device), mels.to(device), dur.to(device)
-
-        with torch.no_grad(): _, gta, _ = model(x, mels, dur)
-
-        gta = gta.cpu().numpy()
-
-        for j, item_id in enumerate(ids):
-            mel = gta[j][:, :mel_lens[j]]
-            mel = (mel + 4) / 8
-            np.save(save_path/f'{item_id}.npy', mel, allow_pickle=False)
-
-        bar = progbar(i, iters)
-        msg = f'{bar} {i}/{iters} Batches '
-        stream(msg)
-
-
-if __name__ == "__main__":
-    main()
+    trainer = ForwardTrainer(paths)
+    trainer.train(model, optimizer)
--- train_tacotron.py
+++ train_tacotron.py
@@ -1,24 +1,77 @@
+import argparse
+import itertools
+from pathlib import Path
+
 import torch
 from torch import optim
-import torch.nn.functional as F
-from utils import hparams as hp
-from utils.display import *
-from utils.dataset import get_tts_datasets
-from utils.paths import Paths
+from torch.utils.data.dataloader import DataLoader
 from models.tacotron import Tacotron
-import argparse
-from utils import data_parallel_workaround
-from pathlib import Path
-import time
-import numpy as np
-from utils.checkpoints import save_checkpoint, restore_checkpoint
+from trainer.taco_trainer import TacoTrainer
+from utils import hparams as hp
+from utils.checkpoints import restore_checkpoint
+from utils.dataset import get_tts_datasets
+from utils.display import *
+from utils.dsp import np_now
+from utils.paths import Paths
 from utils.text import phonemes
 
 
-def np_now(x: torch.Tensor): return x.detach().cpu().numpy()
+def create_gta_features(model: Tacotron,
+                        train_set: DataLoader,
+                        val_set: DataLoader,
+                        save_path: Path):
+    model.eval()
+    device = next(model.parameters()).device  # use same device as model parameters
+    iters = len(train_set) + len(val_set)
+    dataset = itertools.chain(train_set, val_set)
+    for i, (x, mels, ids, mel_lens) in enumerate(dataset, 1):
+        x, mels = x.to(device), mels.to(device)
+        with torch.no_grad():
+            _, gta, _ = model(x, mels)
+        gta = gta.cpu().numpy()
+        for j, item_id in enumerate(ids):
+            mel = gta[j][:, :mel_lens[j]]
+            mel = (mel + 4) / 8
+            np.save(str(save_path/f'{item_id}.npy'), mel, allow_pickle=False)
+        bar = progbar(i, iters)
+        msg = f'{bar} {i}/{iters} Batches '
+        stream(msg)
 
 
-def main():
+def create_align_features(model: Tacotron,
+                          train_set: DataLoader,
+                          val_set: DataLoader,
+                          save_path: Path):
+    assert model.r == 1, f'Reduction factor of tacotron must be 1 for creating alignment features! ' \
+                         f'Reduction factor was: {model.r}'
+    model.eval()
+    device = next(model.parameters()).device  # use same device as model parameters
+    iters = len(val_set) + len(train_set)
+    dataset = itertools.chain(train_set, val_set)
+    for i, (x, mels, ids, mel_lens) in enumerate(dataset, 1):
+        x, mels = x.to(device), mels.to(device)
+        with torch.no_grad():
+            _, _, attn = model(x, mels)
+        attn = np_now(attn)
+        bs, chars = attn.shape[0], attn.shape[2]
+        argmax = np.argmax(attn[:, :, :], axis=2)
+        mel_counts = np.zeros(shape=(bs, chars), dtype=np.int32)
+        for b in range(attn.shape[0]):
+            # fix random jumps in attention 
+            for j in range(1, argmax.shape[1]):
+                if abs(argmax[b, j] - argmax[b, j-1]) > 10:
+                    argmax[b, j] = argmax[b, j-1]
+            count = np.bincount(argmax[b, :mel_lens[b]])
+            mel_counts[b, :len(count)] = count[:len(count)]
+
+        for j, item_id in enumerate(ids):
+            np.save(str(save_path / f'{item_id}.npy'), mel_counts[j, :], allow_pickle=False)
+        bar = progbar(i, iters)
+        msg = f'{bar} {i}/{iters} Batches '
+        stream(msg)
+
+
+if __name__ == '__main__':
     # Parse Arguments
     parser = argparse.ArgumentParser(description='Train Tacotron TTS')
     parser.add_argument('--force_train', '-f', action='store_true', help='Forces the model to train past total steps')
@@ -61,168 +114,32 @@ def main():
                      dropout=hp.tts_dropout,
                      stop_threshold=hp.tts_stop_threshold).to(device)
 
-
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
-    print(f'num params {params}')
+    print(f'Num Params: {params}')
 
     optimizer = optim.Adam(model.parameters())
-    restore_checkpoint('tts', paths, model, optimizer, create_if_missing=True)
-
-    if not force_gta and not force_align:
-        for i, session in enumerate(hp.tts_schedule):
-            current_step = model.get_step()
-            r, lr, max_step, batch_size = session
-            training_steps = max_step - current_step
-
-            # Do we need to change to the next session?
-            if current_step >= max_step:
-                # Are there no further sessions than the current one?
-                if i == len(hp.tts_schedule)-1:
-                    # There are no more sessions. Check if we force training.
-                    if force_train:
-                        # Don't finish the loop - train forever
-                        training_steps = 999_999_999
-                    else:
-                        # We have completed training. Breaking is same as continue
-                        break
-                else:
-                    # There is a following session, go to it
-                    continue
-
-            model.r = r
-
-            simple_table([(f'Steps with r={r}', str(training_steps//1000) + 'k Steps'),
-                            ('Batch Size', batch_size),
-                            ('Learning Rate', lr),
-                            ('Outputs/Step (r)', model.r)])
-
-            train_set, val_set = get_tts_datasets(paths.data, batch_size, r)
-            tts_train_loop(paths, model, optimizer, train_set, lr, training_steps, val_set)
-
-        print('Training Complete.')
-        print('To continue training increase tts_total_steps in hparams.py or use --force_train\n')
+    restore_checkpoint('tts', paths, model, optimizer, create_if_missing=True, device=device)
 
     if force_gta:
         print('Creating Ground Truth Aligned Dataset...\n')
-        train_set, attn_example = get_tts_datasets(paths.data, 8, model.r)
-        create_gta_features(model, train_set, paths.gta)
+        train_set, val_set = get_tts_datasets(paths.data, 8, model.r)
+        create_gta_features(model, train_set, val_set, paths.gta)
         print('\n\nYou can now train WaveRNN on GTA features - use python train_wavernn.py --gta\n')
-
-    if force_align:
+    elif force_align:
         print('Creating Attention Alignments...\n')
-        train_set, attn_example = get_tts_datasets(paths.data, 8, model.r)
-        create_align_features(model, train_set, paths.alg)
+        train_set, val_set = get_tts_datasets(paths.data, 8, model.r)
+        create_align_features(model, train_set, val_set, paths.alg)
         print('\n\nYou can now train ForwardTacotron - use python train_forward.py\n')
+    else:
+        trainer = TacoTrainer(paths)
+        trainer.train(model, optimizer)
 
 
 
 
-def tts_train_loop(paths: Paths, model: Tacotron, optimizer, train_set, lr, train_steps, attn_example):
-    device = next(model.parameters()).device  # use same device as model parameters
-
-    for g in optimizer.param_groups: g['lr'] = lr
-
-    total_iters = len(train_set)
-    epochs = train_steps // total_iters + 1
-
-    for e in range(1, epochs+1):
-
-        start = time.time()
-        running_loss = 0
-
-        # Perform 1 epoch
-        for i, (x, m, ids, _) in enumerate(train_set, 1):
-
-            x, m = x.to(device), m.to(device)
-
-            # Parallelize model onto GPUS using workaround due to python bug
-            if device.type == 'cuda' and torch.cuda.device_count() > 1:
-                m1_hat, m2_hat, attention = data_parallel_workaround(model, x, m)
-            else:
-                m1_hat, m2_hat, attention = model(x, m)
-
-            m1_loss = F.l1_loss(m1_hat, m)
-            m2_loss = F.l1_loss(m2_hat, m)
-
-            loss = m1_loss + m2_loss
-
-            optimizer.zero_grad()
-            loss.backward()
-            if hp.tts_clip_grad_norm is not None:
-                grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), hp.tts_clip_grad_norm)
-                if np.isnan(grad_norm):
-                    print('grad_norm was NaN!')
-
-            optimizer.step()
-
-            running_loss += loss.item()
-            avg_loss = running_loss / i
-
-            speed = i / (time.time() - start)
-
-            step = model.get_step()
-            k = step // 1000
-
-            if step % hp.tts_checkpoint_every == 0:
-                ckpt_name = f'taco_step{k}K'
-                save_checkpoint('tts', paths, model, optimizer,
-                                name=ckpt_name, is_silent=True)
-
-            if attn_example in ids:
-                idx = ids.index(attn_example)
-                save_attention(np_now(attention[idx][:, :160]), paths.tts_attention/f'{step}')
-                save_spectrogram(np_now(m2_hat[idx]), paths.tts_mel_plot/f'{step}', 600)
-
-            msg = f'| Epoch: {e}/{epochs} ({i}/{total_iters}) | Loss: {avg_loss:#.4} | {speed:#.2} steps/s | Step: {k}k | '
-            stream(msg)
-
-        # Must save latest optimizer state to ensure that resuming training
-        # doesn't produce artifacts
-        save_checkpoint('tts', paths, model, optimizer, is_silent=True)
-        model.log(paths.tts_log, msg)
-        print(' ')
 
 
-def create_gta_features(model: Tacotron, train_set, save_path: Path):
-    device = next(model.parameters()).device  # use same device as model parameters
-    iters = len(train_set)
-    for i, (x, mels, ids, mel_lens) in enumerate(train_set, 1):
-        x, mels = x.to(device), mels.to(device)
-        with torch.no_grad():
-            _, gta, _ = model(x, mels)
-        gta = gta.cpu().numpy()
-        for j, item_id in enumerate(ids):
-            mel = gta[j][:, :mel_lens[j]]
-            mel = (mel + 4) / 8
-            np.save(save_path/f'{item_id}.npy', mel, allow_pickle=False)
-        bar = progbar(i, iters)
-        msg = f'{bar} {i}/{iters} Batches '
-        stream(msg)
 
 
-def create_align_features(model: Tacotron, train_set, save_path: Path):
-    assert model.r == 1, f'Reduction factor of tacotron must be 1 for creating alignment features! ' \
-                         f'Reduction factor was: {model.r}'
-    device = next(model.parameters()).device  # use same device as model parameters
-    iters = len(train_set)
-    for i, (x, mels, ids, mel_lens, _) in enumerate(train_set, 1):
-        x, mels = x.to(device), mels.to(device)
-        with torch.no_grad():
-            _, _, attn = model(x, mels)
-        attn = np_now(attn)
-        bs, chars = attn.shape[0], attn.shape[2]
-        argmax = np.argmax(attn[:, :, :], axis=2)
-        mel_counts = np.zeros(shape=(bs, chars), dtype=np.int32)
-        for b in range(attn.shape[0]):
-            count = np.bincount(argmax[b, :])
-            mel_counts[b, :len(count)] = count[:len(count)]
-        for j, item_id in enumerate(ids):
-            np.save(save_path / f'{item_id}.npy', mel_counts[j, :], allow_pickle=False)
-        bar = progbar(i, iters)
-        msg = f'{bar} {i}/{iters} Batches '
-        stream(msg)
 
-
-if __name__ == "__main__":
-    main()
