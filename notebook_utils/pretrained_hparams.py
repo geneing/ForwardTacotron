e2bf314cace255e08c2a6f30a57d5a1b888ae870
cschaefer26 <c.schaefer.home@gmail.com>
2020-07-08 13:00:22 +0200

    Adjusted pretrained hparams.

--- notebook_utils/pretrained_hparams.py
+++ notebook_utils/pretrained_hparams.py
@@ -40,7 +40,7 @@ n_val = 200                         # num validatino samples
 
 # Model Hparams
 voc_mode = 'RAW'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
-voc_upsample_factors = (5, 5, 11)   # NB - this needs to correctly factorise hop_length
+voc_upsample_factors = (4, 8, 8)   # NB - this needs to correctly factorise hop_length
 voc_rnn_dims = 512
 voc_fc_dims = 512
 voc_compute_dims = 128
